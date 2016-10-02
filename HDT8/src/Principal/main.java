506f7200cd6c977fc866ec92276c131dfb5fa8d6
fuentesmarlon <fue15240@uvg.edu.gt>
2016-10-02 00:18:15 -0600

    Update main.java

--- HDT8/src/Principal/main.java
+++ HDT8/src/Principal/main.java
@@ -12,5 +12,19 @@ Carnets: 15240-
 public class main {
    public static void main(String[] args) {
         // TODO code application logic here
+          BufferedReader br = null;
+       try{
+           String lineas[] = null;
+           String linea;
+           br = new BufferedReader(new FileReader(""));
+           while((linea = br.readLine())!= null){
+               linea.split(",");
+               
+           }
+       }catch(IOException e ){
+           e.printStackTrace();
+       }
+    
+   
    }
 }
