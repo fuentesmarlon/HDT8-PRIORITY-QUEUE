920434d790f8addd3effeb2c1af78ac4cb40c0a5
Marlon <Marlon@Marlon-PC>
2016-10-02 00:13:14 -0600

    v1

---
+++ HDT8/src/Principal/main.java
@@ -0,0 +1,16 @@
+/*
+Universidad del Valle de Guatemala 
+Algoritmos y Estructura de datos
+Nombres: Marlon Fuentes- Mario Hernandez
+Carnets: 15240- 
+*/
+
+/**
+ *
+ * @author Marlon
+ */
+public class main {
+   public static void main(String[] args) {
+        // TODO code application logic here
+   }
+}
