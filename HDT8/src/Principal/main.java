2a3fa1c6ab28d72a656a28e47bd278f7b641599e
Marioh96 <her15135@uvg.edu.gt>
2016-10-03 21:52:00 -0600

    Update main.java

--- HDT8/src/Principal/main.java
+++ HDT8/src/Principal/main.java
@@ -6,7 +6,7 @@ Carnets: 15240- 15135
 */
 /**
  *
- * @author Marlon
+ * @author Marlon y Mario
  */
 public class main {
    public static void main(String[] args) {
@@ -26,4 +26,56 @@ public class main {
     
    
    }
+   private void principal(){
+      inicializar();
+   }
+   private void inicializar(){
+      String respuesta="";
+		boolean val = true;
+		
+		System.out.println("Bienvenido al Centro Medico! \n");
+		do{
+			System.out.println("Menu:");
+			System.out.println("1. Pacientes Ingresados");
+			System.out.println("2. Ingresar Paciente");
+			System.out.println("3. Out\n");
+			
+			System.out.print("---- ");
+			resp = snr.nextLine();
+			switch(respuesta){
+				case "1":
+					System.out.println("Los Pacientes que se encuentran en la lista son:");
+					System.out.println("__________________________________");
+					for(Pacientes p:Pacientes.getRazon().getNombre()){
+						System.out.println("Nombre: "+p.getNombre());
+						System.out.println("Sintoma: "+p.getRazon());
+						System.out.println("Codigo: "+p.getPrioridad());
+						System.out.println("__________________________________");
+					}
+					
+					break;
+				case "2":
+					Pacientes p = new Pacientes();
+					System.out.println("Ingrese la informacion del paciente:");
+					System.out.print("Nombre: ");
+					respuesta = snr.nextLine();
+					p.setName(respuesta);
+					System.out.print("Sintoma: ");
+					respuesta = snr.nextLine();
+					p.setSintoma(respuesta);
+					System.out.print("Codigo: ");
+					respuesta = snr.nextLine();
+					p.setCodigo(respuesta);
+					Pacientes.getRazon().getNombre().add(p);
+					break;
+				case "e":
+					System.out.println("Gracias por su preferencia!");
+					val = false;
+					break;
+				default:
+					System.out.println("No es una opccion!\n");
+					break;
+			}
+			
+		}while(val);
 }
